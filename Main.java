6619bac10b77b7636fd72048f24025b540de71db
nurbolot.dolatbekovich@gmail.com <nurbolot.dolatbekovich@gmail.com>
2024-03-29 14:57:02 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+
+        tree.add(42);
+        tree.add(44);
+        tree.add(43);
+        tree.add(36);
+        tree.add(16);
+        tree.add(2);
+        tree.add(1);
+        tree.add(10);
+        tree.add(6);
+        tree.add(39);
+        tree.add(20);
+        tree.add(38);
+        tree.add(32);
+        tree.add(17);
+        tree.add(30);
+
+
+        tree.print();
+
+    }
+}
